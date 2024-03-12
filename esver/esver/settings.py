51eed47504d157843dfc67bd88f130142e263e7e
Dharshini T <dharshinit0303@gmail.com>
2024-03-12 23:35:39 +0530

    trip app intiallization

--- esver/esver/settings.py
+++ esver/esver/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'esver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_esver',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '' ,
+        'PORT':'3306'
     }
 }
 
