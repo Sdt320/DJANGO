bf7346b224ff969cb5310f61b9ac286fb3fa3d92
Dharshini T <dharshinit0303@gmail.com>
2024-03-16 21:33:06 +0530

    page 1

--- esver/trip/admin.py
+++ esver/trip/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+
+class CatagoryAdmin(admin.ModelAdmin):
+    list_display=('name','image','descripition')
+
+admin.site.register(Catagory,CatagoryAdmin)
+admin.site.register(Product)
 
 # Register your models here.
--- esver/trip/models.py
+++ esver/trip/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+import datetime
+import os
+
+
 
 # Create your models here.
+def getFileName(request,filename):             #file name 
+    now_time=datetime.datetime.now().strftime("%Y%m%d%H:%M:%S")
+    new_filename="%s%s"%(now_time,filename)
+    return os.path.join('uploads/',new_filename)
+
+class Catagory(models.Model):
+    name=models.CharField(max_length=150,null=False,blank=False)
+    image=models.ImageField(upload_to=getFileName,null=True,blank=True)
+    descripition=models.TextField(max_length=500,null=False,blank=False)
+    status=models.BooleanField(default=False,help_text="0-show,1-Hidden")
+    created_at=models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return self.name
+
+class Product(models.Model):
+    catagory=models.ForeignKey(Catagory,on_delete=models.CASCADE)
+    name=models.CharField(max_length=150,null=False,blank=False)
+    vendor=models.CharField(max_length=150,null=False,blank=False)    
+    product_image=models.ImageField(upload_to=getFileName,null=True,blank=True)
+    quantity=models.IntegerField(null=False,blank=False)
+    original_price=models.FloatField(null=False,blank=False)
+    selling_price=models.FloatField(null=False,blank=False)
+    descripition=models.TextField(max_length=500,null=False,blank=False)
+    status=models.BooleanField(default=False,help_text="0-show,1-Hidden")
+    trending=models.BooleanField(default=False,help_text="0-default,1-Trending")
+    created_at=models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return self.name
