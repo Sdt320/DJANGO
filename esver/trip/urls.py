f18d8ed97a1c9030fece21deb298d0e3e7536bee
Dharshini T <dharshinit0303@gmail.com>
2024-04-01 15:44:22 +0530

    user registration ok

--- esver/trip/urls.py
+++ esver/trip/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns= [
     path('',views.home,name="home"),
+    path('login',views.login,name="login"),
     path('register',views.register,name="register"),
     path('places',views.places,name="places"),
-    path('places/<str:name>',views.placesview,name="places")
+    path('places/<str:name>',views.placesview,name="places"),
+    path('places/<str:cname>/<str:pname>',views.product_details,name="product_details")
 
 
 ]
--- esver/trip/views.py
+++ esver/trip/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
+from trip.form import CustomUserForm
 from . models import *
 from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request,"trip/index.html")
+
+def login(request):
+    return render(request,"trip/login.html")
+
+
 def register(request):
-    return render(request,"trip/register.html")
+  form=CustomUserForm()
+  if request.method=='POST':
+    form=CustomUserForm(request.POST)
+    if form.is_valid():
+      form.save()
+      messages.success(request,"Registration Success You can Login Now..!")
+      return redirect('/login')
+  return render(request,"trip/register.html",{'form':form})
+
+
 def places(request):
     catagory=Catagory.objects.filter(status=0)
     return render(request,"trip/places.html",{'catagory':catagory})
@@ -15,5 +30,17 @@ def placesview(request,name):
         products=Product.objects.filter(catagory__name=name)
         return render(request,"trip/products/index.html",{'products':products,"category_name":name})
     else:
-        message.warning(request,"No Such Catagory Found")
+        
         return redirect('places')
+
+def product_details(request,cname,pname):
+    if(Catagory.objects.filter(name=cname,status=0)):
+      if(Product.objects.filter(name=pname,status=0)):
+        products=Product.objects.filter(name=pname,status=0).first()
+        return render(request,"trip/products/product_details.html",{"products":products})
+      else:
+        messages.error(request,"No Such Produtct Found")
+        return redirect('collections')
+    else:
+      messages.error(request,"No Such Catagory Found")
+      return redirect('collections')
